825e3939260da7cc073c6ed8170d6690001ac20e
WyomingCPA <WyomingCPA@yandex.ru>
2023-11-13 00:33:14 +0300

    add shedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('command:get-vk-anime')->hourly();
         $schedule->command('command:get-rss-habr')->hourly();
         $schedule->command('command:send-new-post')->everyTwoHours();
     }

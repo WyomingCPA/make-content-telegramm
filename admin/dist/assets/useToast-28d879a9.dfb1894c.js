ee41082d6b1a6e3bf09b1df226d7338756665edd
Nikolay <WyomingCPA@yandex.ru>
2023-11-12 18:35:10 +0300

    fix bug

--- admin/dist/assets/useToast-28d879a9.dfb1894c.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:admin/dist/assets/useToast-28d879a9.dfb1894c.js
-import{n as c,E as l,G as a,H as r,I as n,J as i}from"./index.c3f6c7e2.js";const u=()=>c(()=>{var e,o;return((e=l())==null?void 0:e._context)||((o=a())==null?void 0:o.appContext)}),v=()=>{const e=u(),o=[];return{init:t=>{const s=r(t,e.value);return s&&o.push(s),s},close:t=>n(t),closeAll:(t=!1)=>i(t?void 0:e.value),closeAllCreatedInThisHook:()=>{o.forEach(t=>n(t))}}};export{v as u};
-========
-import{n as c,E as l,G as a,H as r,I as n,J as i}from"./index.bd98a098.js";const u=()=>c(()=>{var e,o;return((e=l())==null?void 0:e._context)||((o=a())==null?void 0:o.appContext)}),v=()=>{const e=u(),o=[];return{init:t=>{const s=r(t,e.value);return s&&o.push(s),s},close:t=>n(t),closeAll:(t=!1)=>i(t?void 0:e.value),closeAllCreatedInThisHook:()=>{o.forEach(t=>n(t))}}};export{v as u};
->>>>>>>> e1f89499ee4ba49ff54d0c8c26d3741ab7a65127:admin/dist/assets/useToast-28d879a9.caa0cd26.js

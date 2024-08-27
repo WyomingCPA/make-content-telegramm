9bccb5e12fbdf0c0f7260c3996f81759cb71bb26
Nikolay <WyomingCPA@yandex.ru>
2024-08-27 23:43:04 +0300

    fix bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,10 +15,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:get-vk-anime')->hourly();
-        $schedule->command('command:get-vk-estetic-vibes')->hourly();
-        $schedule->command('command:get-vk-sexygirl')->hourly();
-        $schedule->command('command:get-vk-mirtlenmai')->hourly();
+        //$schedule->command('command:get-vk-anime')->hourly();
+        //$schedule->command('command:get-vk-estetic-vibes')->hourly();
+        //$schedule->command('command:get-vk-sexygirl')->hourly();
+        //$schedule->command('command:get-vk-mirtlenmai')->hourly();
+        $schedule->command('command:get-vk-post')->hourly();
         $schedule->command('command:get-rss-habr')->hourly();
         $schedule->command('command:send-new-post')->everyTwoHours();
         $schedule->command('command:delete-old-post')->everyTwoHours();
